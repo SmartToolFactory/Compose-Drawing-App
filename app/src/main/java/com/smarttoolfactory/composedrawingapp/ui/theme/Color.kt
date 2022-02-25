94355a23dab92f810a44cb9c9dc901b1d92d92ae
Smart Tool Factory <smarttoolfactory@icloud.com>
2022-02-25 14:19:47 +0300

    add colors

--- app/src/main/java/com/smarttoolfactory/composedrawingapp/ui/theme/Color.kt
+++ app/src/main/java/com/smarttoolfactory/composedrawingapp/ui/theme/Color.kt
@@ -6,3 +6,25 @@ val Purple200 = Color(0xFFBB86FC)
 val Purple500 = Color(0xFF6200EE)
 val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
+
+val backgroundColor = Color(0xffECEFF1)
+
+val Orange400 = Color(0xffFFA726)
+val Blue400 = Color(0xff42A5F5)
+val Pink400 = Color(0xffEC407A)
+val Green400 = Color(0xff66BB6A)
+val Red400 = Color(0xffEF5350)
+val Yellow400 = Color(0xffFFEE58)
+val Purple400 = Color(0xffAB47BC)
+val Brown400 = Color(0xff8D6E63)
+val BlueGrey400 = Color(0xff78909C)
+
+val gradientColors = listOf(
+    Color.Red,
+    Color.Magenta,
+    Color.Blue,
+    Color.Cyan,
+    Color.Green,
+    Color.Yellow,
+    Color.Red
+)
